7924b452a53331503b6568cb54f29c7af3b7a29d
KODA Hiroshi <koda@metaps-payment.com>
2024-01-09 18:51:34 +0900

    $HOME/bin が PATHに設定しているかのチェックを追加

--- setup.sh
+++ setup.sh
@@ -11,6 +11,9 @@ fi
 echo "copy ssh file"
 cp ssh $HOME_BIN_DIR/ssh
 
+echo $PATH|grep "$HOME/bin"  > /dev/null 2>&1
+RET=${?}
+
 # PATHの設定
 if [ "$RET" = "1" ]; then
   if [ "$SHELL" = "/bin/zsh" ]; then
